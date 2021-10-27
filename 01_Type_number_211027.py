d9faf1f9574108547a2a895e730f9b7f82e5e83d
Ryu <ryu.axel@gmail.com>
2021-10-27 20:05:02 +0900

    Study upto 25:51

---
+++ 01_Type_number_211027.py
@@ -0,0 +1,10 @@
+# 숫자 자료형
+print(5)
+print(-10)
+a = 5
+print(a)
+print(3.14)
+print(10000)
+print(a + 7)
+print(a * 1000)
+print(3*(3+1))
---
+++ 03_Type_boolean_211027.py
@@ -0,0 +1,9 @@
+# 참과 거짓을 의미한다. 
+print(5 > 10) # 이렇게 출력하면 거짓이므로 false가 나오게 된다 
+print(5 < 10)
+print(True) # 이렇게 기재하여도 True, 즉 boolean형으로 자동으로 인식 된다. 
+print(False)
+print(not True)
+print(not False)
+# print(! True) 는 안된다. C와는 다르게 !로 사용이 여기선 안되는 듯 보인다. 
+print(not (5 > 19)) # False의 반대니까 True가 나오게 된다. 
